b09d29e000cd2e055d9911bc65266c8b1c66785b
Azhari Marzan <azhari.marzan@gmail.com>
2020-12-05 03:12:06 +0700

    ^ listening port

--- src/index.js
+++ src/index.js
@@ -3,6 +3,6 @@ const app = require('./app');
 const port = process.env.PORT || 5000;
 app.listen(port, () => {
   /* eslint-disable no-console */
-  console.log(`Listening: http://localhost:${port}`);
+  console.log(`Listening on port:${port}`);
   /* eslint-enable no-console */
 });
