4ad75261c9445404a35e4b57aaaccaf8b04332c4
Azhari Marzan <azhari.marzan@gmail.com>
2020-12-05 03:53:31 +0700

    ^ show hadits with paginate

--- src/controllers/haditsController.js
+++ src/controllers/haditsController.js
@@ -11,10 +11,34 @@ exports.index = async (req, res) => {
     }
 };
 
-exports.show = (req, res) => {
-    res.json({
-        message: "show here",
-    });
+exports.show = async (req, res) => {
+    try {
+        let book = req.params.book;
+        checkBook = await Book.findOne({ id: book });
+        if(!checkBook) return errorJson(res, 'Book not found!', 404);
+
+        const page = req.query.page || 1;
+        const limit = req.query.limit || 10;
+        const skip = (page - 1) * limit;
+
+        const total = await Hadit.find({ book }).countDocuments();
+        const hadits = await Hadit.find({ book }).select({"_id": 0, "arab": 1, "id": 1, "number": 1})
+            .skip(skip)
+            .limit(parseInt(limit))
+            .sort([['number', 1]])
+            .exec();
+
+        if(total === 0) return errorJson(res, `Hadith not found`, 404);
+
+        return json(res, {
+            page: parseInt(page),
+            limit: parseInt(limit),
+            last_page: Math.ceil(total / limit),
+            hadits
+        });
+    } catch (error) {
+        return errorJson(res, error);
+    }
 };
 
 exports.detail = (req, res) => {
